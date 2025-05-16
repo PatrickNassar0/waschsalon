405e7b81b62eeb6ef037b76423f364ad72bd0f1d
PatrickNassar0 <patrick.nassar@solactive.com>
2025-05-16 20:32:04 +0200

    Enhance loading state handling for washing and dryer machines in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,44 +66,55 @@ export default function Home() {
     fetchDate()
   }, [])
 
-
-
   return (
     <div className="flex flex-col items-center justify-center min-h-[100dvh] bg-gray-100 space-y-10 p-5">
       <div className="md:flex items-center justify-evenly w-full gap-x-8 space-y-5 md:space-y-0">
-      <h1 className="text-3xl font-bold text-gray-800 text-left">
-        Laundry Room Status
-        <br />
-        Otto-Franke-Straße 45
-      </h1>
-      <LanguageSelect />
+        <h1 className="text-3xl font-bold text-gray-800 text-left">
+          Laundry Room Status
+        </h1>
+        <LanguageSelect />
       </div>
+
       <hr className="w-9/12 border-gray-300" />
+
       <h2 className="text-2xl font-bold text-gray-800">
         Washing Machines
       </h2>
-      <div className="grid md:grid-cols-3 items-center justify-center gap-5">
-        {washingMachines?.map((washingMachine) => (
-          <div key={washingMachine.id}>
-            <Button isActive={washingMachine.availableWashingMachines} onClick={() => handleClickWashingMachine(washingMachine.id, !washingMachine.availableWashingMachines, new Date().toString())}>
-              <ShowText text='Washing Machine' id={washingMachine.id} lastChange={changeDateFormat(washingMachine.lastChange.toString())} available={washingMachine.availableWashingMachines} />
-            </Button>
-          </div>
-        ))}
-      </div>
+      {washingMachines ?
+        <div className="grid md:grid-cols-3 items-center justify-center gap-5">
+          {washingMachines?.map((washingMachine) => (
+            <div key={washingMachine.id}>
+              <Button isActive={washingMachine.availableWashingMachines} onClick={() => handleClickWashingMachine(washingMachine.id, !washingMachine.availableWashingMachines, new Date().toString())}>
+                <ShowText text='Washing Machine' id={washingMachine.id} lastChange={changeDateFormat(washingMachine.lastChange.toString())} available={washingMachine.availableWashingMachines} />
+              </Button>
+            </div>
+          ))}
+        </div>
+        :
+        <div className="w-full text-center">
+          <p className="text-gray-500">Loading...</p>
+        </div>
+      }
       <hr className="w-9/12 border-gray-300" />
+
       <h2 className="text-2xl font-bold text-gray-800">
         Dryer Machines
       </h2>
-      <div className="grid md:grid-cols-4 items-center justify-center gap-5">
-        {dryerMachines?.map((dryerMachine) => (
-          <div key={dryerMachine.id}>
-            <Button isActive={dryerMachine.availableDryerMachines} onClick={() => handleClickDryerMachines(dryerMachine.id, !dryerMachine.availableDryerMachines, new Date().toString())}>
-              <ShowText text='Dryer Machine' id={dryerMachine.id} lastChange={changeDateFormat(dryerMachine.lastChange.toString())} available={dryerMachine.availableDryerMachines} />
-            </Button>
-          </div>
-        ))}
-      </div>
+      {dryerMachines ?
+        <div className="grid md:grid-cols-4 items-center justify-center gap-5">
+          {dryerMachines?.map((dryerMachine) => (
+            <div key={dryerMachine.id}>
+              <Button isActive={dryerMachine.availableDryerMachines} onClick={() => handleClickDryerMachines(dryerMachine.id, !dryerMachine.availableDryerMachines, new Date().toString())}>
+                <ShowText text='Dryer Machine' id={dryerMachine.id} lastChange={changeDateFormat(dryerMachine.lastChange.toString())} available={dryerMachine.availableDryerMachines} />
+              </Button>
+            </div>
+          ))}
+        </div>
+        :
+        <div className="w-full text-center">
+          <p className="text-gray-500">Loading...</p>
+        </div>
+      }
     </div>
   );
 }
